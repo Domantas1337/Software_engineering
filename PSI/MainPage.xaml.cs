9f2aeb896292eb22fbb6076b67c9526a877b900d
Ignas <ignas.jogminas@mif.stud.vu.lt>
2022-09-10 10:28:35 +0300

    + MAUI template

---
+++ PSI/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace PSI;
+
+public partial class MainPage : ContentPage
+{
+	private int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void onCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+		if (count == 1)
+			counterBtn.Text = $"Clicked {count} time";
+		else
+			counterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(counterBtn.Text);
+	}
+
+	private void onTextChanged(object sender, EventArgs e)
+	{
+		hiLabel.Text = myEditor.Text;
+	}
+}
