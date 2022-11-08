5cdb1a94afb1148368f9ee9ae9880826a3f9f18f
Domantas Kakčiukas <40541109+Domantas1337@users.noreply.github.com>
2022-11-08 14:21:53 +0200

    time spent

--- PSI/Views/ReportDetailPage.xaml.cs
+++ PSI/Views/ReportDetailPage.xaml.cs
@@ -4,16 +4,34 @@ namespace PSI.Views;
 
 public partial class ReportDetailPage : ContentPage
 {
-	public ReportDetailPage(DetailViewModel vm)
+
+    private int timePassed = 0;
+    public ReportDetailPage(DetailViewModel vm)
 	{
 		InitializeComponent();
 		BindingContext = vm;
 
-		Debug.WriteLine("Time on ReportDetailPage: " + vm.currentTime);
+
+        System.Timers.Timer aTimer = new System.Timers.Timer();
+        aTimer.Elapsed += new System.Timers.ElapsedEventHandler(OnTimedEvent);
+        aTimer.Interval = 5000;
+        aTimer.Enabled = true;
+
+        Debug.WriteLine("Time on ReportDetailPage: " + vm.currentTime);
 	}
 
 	public void OnReportScrolled(object sender, ScrolledEventArgs e)
 	{
+
+       
+
         Debug.WriteLine($"ScrollX: {e.ScrollX}, ScrollY: {e.ScrollY}");
+    }
+
+    public void OnTimedEvent(object source, System.Timers.ElapsedEventArgs e)
+    {
+        timePassed += 5;
+        
+        Debug.WriteLine("Time passed: " + timePassed);
     }
 }
